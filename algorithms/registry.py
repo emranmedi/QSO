c99f26d757dd055334f61e95acbc4b89976f0eef
emranmedi <emran.medi@gmail.com>
2026-07-01 11:55:29 +0800

    Create registry.py

---
+++ algorithms/registry.py
@@ -0,0 +1,11 @@
+"""
+QSO Research — Competitor Algorithm Registry
+All 15 algorithms with unified interface
+Generated from 02_Competitors.ipynb
+"""
+
+ALGORITHM_NAMES = ['QSO', 'PSO', 'GA', 'DE', 'GWO', 'WOA', 'SCA', 'HHO', 'MPA', 'BFO', 'QBSO', 'QBHO', 'DBO', 'POA', 'EVO']
+
+def get_algorithms():
+    """Returns list of all algorithm names."""
+    return ALGORITHM_NAMES
